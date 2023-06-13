ae1fa28431bc34fbded8d22120a1f470e56298e7
vaibhav patel <workwithvaibhav91@gmail.com>
2023-06-13 19:15:15 +0530

    version 2: final version

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,30 +1,31 @@
 import React from 'react'
 import styled from 'styled-components'
+import Fade from 'react-reveal/Fade'
 
 function Section(props) {
     return (
         <Wrap bgImage={props.backgroundImg}>
-
-            <ItemText>
-                <h1>{props.title}</h1>
-                <p>{props.description}</p>
-            </ItemText>
+            <Fade bottom>
+                <ItemText>
+                    <h1>{props.title}</h1>
+                    <p>{props.description}</p>
+                </ItemText>
+            </Fade>
 
             <Buttons>
-                <ButtonGroup>
-                    <LeftButton>
-                        {props.leftBtnText}
-                    </LeftButton>
-                    {props.RightBtnText &&
-                        <RightButton>
-                            {props.RightBtnText}
-                        </RightButton>
-                    }
-
-                </ButtonGroup>
-
+                <Fade bottom>
+                    <ButtonGroup>
+                        <LeftButton>
+                            {props.leftBtnText}
+                        </LeftButton>
+                        {props.RightBtnText &&
+                            <RightButton>
+                                {props.RightBtnText}
+                            </RightButton>
+                        }
+                    </ButtonGroup>
+                </Fade>
                 <DownArrow src="/images/down-arrow.svg" />
-
             </Buttons>
 
         </Wrap>
@@ -34,6 +35,7 @@ function Section(props) {
 export default Section
 
 const Wrap = styled.div`
+    z-index: 10;
     width: 100vw;
     height: 100vh;
     background-size: cover;
